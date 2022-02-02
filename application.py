f5932bf69cc2e7bc15e16cd3bfc5190f18de65a0
chinmay29hub <chinmaysonawane57@gmail.com>
2022-02-02 19:18:16 +0530

    ui

---
+++ application.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+application = app = Flask(__name__)
+
+@application.route("/")
+def hello():
+    return render_template('index.html')
+
+@application.route("/echo", methods=['POST'])
+def message():
+    mes = request.form['text']
+    message = "You said: " + mes
+    return render_template('index.html', message=message)
+    
+@application.route("/<string:name>")
+def api(name):
+    return "<h1>Hello {} this is a flask app</h1>".format(name)
+
+if __name__ == '__main__':
+    application.run()
