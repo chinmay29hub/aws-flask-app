4c08ba632257c77673240650fc6930ca4620be3d
chinmay29hub <chinmaysonawane57@gmail.com>
2022-02-03 14:48:45 +0530

    deleted zip

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 application = app = Flask(__name__)
 
 @application.route("/")
@@ -7,13 +7,18 @@ def hello():
 
 @application.route("/", methods=['POST'])
 def message():
-    mes = request.form['text']
-    message = "You said: " + mes
-    return render_template('index.html', message=message)
+    if request.method == 'POST':
+        mes = request.form['text']
+        message = "You said: " + mes
+        return render_template('index.html', message=message)
     
 @application.route("/<string:name>")
 def api(name):
     return "<h1>Hello {} this is a flask app</h1>".format(name)
 
+@app.route('/download')
+def download():
+    return send_file('zip/app.zip', as_attachment=True)
+
 if __name__ == '__main__':
-    application.run()
+    application.run(debug=True)
